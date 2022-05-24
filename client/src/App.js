a2578c3ae844790a1382499de37559ddbe2f2b43
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-23 19:44:28 -0700

    +display

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from "react"
+import { useLazyQuery } from "@apollo/client"
+import SearchGif from './queries/searchGif'
 import './App.css';
 
 function App() {
+  const [term, setInput] = useState('')
+  const [searchGif, { data }] = useLazyQuery(SearchGif, {
+    variables: { term }
+  })
+  const dataRef = useRef(data)
+  const [gifs, setGifs] = useState('')
+  useEffect(() => {
+    if (data !== dataRef.current) {
+      const display = data.searchGif.map(obj => {
+        return(
+          <img src={obj}></img>
+        )
+      })
+      setGifs(display)
+    }
+  }, [data, dataRef])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={e => {
+        e.preventDefault()
+        searchGif()
+      }}>
+        <input onChange={e => {
+          setInput(e.target.value)
+        }}></input>
+        <button type='submit'>Submit</button>
+      </form>
+      <div>{gifs}</div>
     </div>
   );
 }
