b28512fd880abd3b6d1fc1277367bcf6c4bdb2d1
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-05-23 17:51:33 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+// imports
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
+const cors = require('cors')
+const request = require('request-promise')
+const path = require('path')
+require('dotenv').config()
+
+// schema
+const schema = buildSchema(`
+    scalar JSON
+    type Query {
+        searchGif(term: String!): JSON!
+    }
+`)
+
+// resolvers
+const root = {
+    searchGif: async (term) => {
+        return await request(`https://g.tenor.com/v1/search?q=${term}&limit=10&key=${process.env.API_KEY}`)
+        .then(res => {
+            const JSONRes = JSON.parse(res)
+            // console.log(JSONRes.results)
+            return JSONRes.results.map(obj => obj.media[0].gif.url)
+        }).catch(console.error)
+    }
+}
+
+// app
+const app = express()
+app.use(cors())
+
+// route
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+// start
+const port = 4000
+app.listen(port, () => {
+    console.log('Running on port:'+port)
+    console.log(`http://localhost:${port}/graphql`)
+})
